3aaa303dfd5f9746a1ebce29a251cfb27968e78a
ssingh52 <shefali_singh@intuit.com>
2021-02-11 19:47:42 +0530

    Added changes

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export class Footer extends Component {
+    render() {
+      return (
+          <footer>
+            
+          </footer>
+  
+      )
+    }
+  }
